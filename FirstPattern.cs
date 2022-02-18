b1d442fa80d0f77d5cb1542baa3707baad0e30c2
JOY <joybosu@gmail.com>
2022-02-19 01:50:41 +0530

    number pattern

---
+++ FirstPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternShow
+{
+    public class PaternShow
+    {
+        public static void NumberPattern()
+        {
+            Console.WriteLine("Pleas enter the no of Rows...:");
+            int noOfRows=Convert.ToInt32(Console.ReadLine());
+            for(int row=1; row<=noOfRows; row++)
+            {
+                for(int colum=1;colum<=noOfRows; colum++)
+                {
+                    Console.WriteLine(row +" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
