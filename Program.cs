5ce43c603892422ad7f86d8ad0eef5b985d4208e
JOY <joybosu@gmail.com>
2022-02-19 01:35:27 +0530

    Welcome to pattern

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaternShow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Pattern Solve");
+        }
+    }
+}
